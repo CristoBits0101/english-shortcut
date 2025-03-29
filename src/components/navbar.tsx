4e225150b8f50c060b5076d4a6637327224e41d4
Cristo Suárez <cristobits0101@gmail.com>
2025-03-29 11:33:10 +0000

    Update navbar and header layout for improved spacing and structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export default function NavbarComponent() {
     return (
-        <nav>
+        <nav className='flex flex-col gap-2'>
             <h2>Phonetics</h2>
-            <h2>Verb Tenses</h2>
             <h2>Vocabulary</h2>
+            <h2>Verb Tenses</h2>
         </nav>
     )
 }
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -9,7 +9,7 @@ import { dancingScript } from "@/lib/google-fonts";
 
 export default function HeaderLayout() {
     return (
-        <header className='h-full p-4 flex flex-col gap-4 bg-blue-500'>
+        <header className='h-full p-4 flex flex-col gap-8 bg-blue-500'>
             <h1 className={`${dancingScript.className} text-4xl font-medium`}>
                 English Shortcut
             </h1>
