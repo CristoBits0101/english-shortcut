4b569daae0726eafab9430770afa5f270cce9229
Cristo Suárez <cristobits0101@gmail.com>
2025-03-29 11:28:43 +0000

    Update header layout to use custom font and enhance title styling

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -4,10 +4,14 @@ import Navbar from '@/components/navbar'
 // Dependencies
 import React from 'react'
 
+// Fonts
+import { dancingScript } from "@/lib/google-fonts";
+
 export default function HeaderLayout() {
     return (
         <header className='h-full p-4 flex flex-col gap-4 bg-blue-500'>
-            <h1 className='text-2xl font-medium'>English Shortcut</h1>
+            <h1 className={`${dancingScript.className} text-4xl font-medium`}
+            >English Shortcut</h1>
             <Navbar />
         </header>
     )
