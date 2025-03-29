c7cbf45225abd0633eef198dae345743a15d9f4a
Cristo Suárez <cristobits0101@gmail.com>
2025-03-29 16:13:12 +0000

    Remove background color from MainLayout for improved styling consistency

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -3,6 +3,6 @@ import { ReactNode } from 'react';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <main className='h-full bg-red-500'>{children}</main>
+    <main className='h-full'>{children}</main>
   );
 }
