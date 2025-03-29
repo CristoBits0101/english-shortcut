d49e6d688516d99c0218c111fc09267ac2f883a3
Cristo Suárez <cristobits0101@gmail.com>
2025-03-29 11:29:17 +0000

    Fix header layout formatting for improved readability

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -10,8 +10,9 @@ import { dancingScript } from "@/lib/google-fonts";
 export default function HeaderLayout() {
     return (
         <header className='h-full p-4 flex flex-col gap-4 bg-blue-500'>
-            <h1 className={`${dancingScript.className} text-4xl font-medium`}
-            >English Shortcut</h1>
+            <h1 className={`${dancingScript.className} text-4xl font-medium`}>
+                English Shortcut
+            </h1>
             <Navbar />
         </header>
     )
